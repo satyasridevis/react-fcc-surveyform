502cff0c8a525a18a2b031439e80a4587a9a4506
satyasridevis <satyasridevisk@gmail.com>
2023-03-03 10:43:25 +0530

    finished: header section

---
+++ src/components/form/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './form.css'
+
+const Form = () => {
+  return (
+      <div className='form-container'>
+          <form className='form-box'>
+              form
+         </form>
+    </div>
+  )
+}
+
+export default Form
